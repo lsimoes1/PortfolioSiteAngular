72af04ff963715177b18bdcaa92da86110657f05
Luan Simões <32942858+lsimoes1@users.noreply.github.com>
2019-04-17 20:38:38 -0300

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule, Component } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { WorkComponent } from './work/work.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'work', component: WorkComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
