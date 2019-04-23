d274569f145d484e5c62ac471abc20af9769f8b5
Luan Simões <luan.simoes1@gmail.com>
2019-04-23 16:48:21 -0300

    Inclusão de um arquivo json dos cursos.

--- src/app/academy/academy.component.ts
+++ src/app/academy/academy.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import data from './cursos.json';
+
+// const cursos = data.cursos;
+
 @Component({
   selector: 'app-academy',
   templateUrl: './academy.component.html',
-  styleUrls: ['./academy.component.css']
+  styleUrls: ['./academy.component.css'],
 })
-export class AcademyComponent implements OnInit {
 
-  constructor() { }
+export class AcademyComponent {
 
-  ngOnInit() {
-  }
-
+  cursos = data.cursos;
+  
 }
