2d9fcae6aaff0577bdf397da1b19a4cfcba7cb05
Luan Simões <luan.simoes1@gmail.com>
2019-04-24 16:33:34 -0300

    Alteração no texto.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent  {
 
-  constructor() { }
+  p1 = `Meu nome é Luan, tenho atualmente 27 anos.
+    Em 2014 iniciei meus estudos e trabalhos na área da tecnologia. Atuei na area de Suporte auxiliando
+    lojas na instalação de sua ferramenta transacional e na integração com PDVs. `
+    
+  p2 = `Em 2015 tive a oportunidade de me desenvolver na área de Produção e Monitoramento realizando consultas,
+    alterações e extrações de relatório diretamente no banco de dados SQL Server, monitorações em servidores,
+    IIS e todo ambiente transacional. Nesse período obtive um pequeno conhecimento em Reporting Service também!`
 
-  ngOnInit() {
-  }
-
+  p3 = `Atualmente exerço a função no setor de sustentação sendo de nossa competência a correção de bugs em
+    sistemas internos nas aplicações Windows, Web, Windows Services e banco de dados Sql Server. As linguagens as quais utilizamos
+    são: C#, Visual Basic, Asp.Net, SQL Server e Object Pascal (Delphi).`
 }
