8f0780fa5463d99487fb399edec27f59bfc5d29d
Luan <luan.simoes1@gmail.com>
2019-04-28 11:15:01 -0300

    Ajuste da idade e dos contatos.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,21 +1,17 @@
 import { Component } from '@angular/core';
+import { isDate, toDate } from '@angular/common/src/i18n/format_date';
+import { DatePipe } from '@angular/common';
+import { initDomAdapter } from '@angular/platform-browser/src/browser';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent  {
 
-  p1 = `Meu nome é Luan, tenho atualmente 27 anos.
-    Em 2014 iniciei meus estudos e trabalhos na área da tecnologia. Atuei na area de Suporte auxiliando
-    lojas na instalação de sua ferramenta transacional e na integração com PDVs. `
-    
-  p2 = `Em 2015 tive a oportunidade de me desenvolver na área de Produção e Monitoramento realizando consultas,
-    alterações e extrações de relatório diretamente no banco de dados SQL Server, monitorações em servidores,
-    IIS e todo ambiente transacional. Nesse período obtive um pequeno conhecimento em Reporting Service também!`
-
-  p3 = `Atualmente exerço a função no setor de sustentação sendo de nossa competência a correção de bugs em
-    sistemas internos nas aplicações Windows, Web, Windows Services e banco de dados Sql Server. As linguagens as quais utilizamos
-    são: C#, Visual Basic, Asp.Net, SQL Server e Object Pascal (Delphi).`
+  anoNascimento = 1992;
+  anoAtual = new Date().getFullYear();
+  idade = this.anoAtual  - this.anoNascimento;
 }
