9f94d3e2ba9bab7d61723d25c596190ee6643800
Luan Simões <luan.simoes1@gmail.com>
2021-02-03 13:26:46 -0300

    Implementação da rota Academy

--- src/app/academy/academy.component.ts
+++ src/app/academy/academy.component.ts
@@ -1,4 +1,6 @@
+import { AcademyService } from './../services/academy.service';
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2/dist/sweetalert2.js'; 
 
 import data from './cursos.json';
 
@@ -10,10 +12,42 @@ import data from './cursos.json';
   styleUrls: ['./academy.component.css'],
 })
 
-export class AcademyComponent {
+export class AcademyComponent implements OnInit {
 
   cursosTI = data.cursosTI;
   cursoIdiomas = data.cursosIdiomas;
-  cursoGraduacao = data.graduacao;
-  
+  cursoGraduacao = data.graduacao;s
+  loading:boolean;
+
+  constructor(private AcademyService: AcademyService) { }
+
+  ngOnInit(){
+    this.getInfoAcademy();
+  }
+
+
+  getInfoAcademy(){
+    this.AcademyService.getAuthToken().subscribe((token) => {
+      this.AcademyService.getInfoAcademy(token.accessToken).subscribe((response) => {
+        console.log(response)
+        this.loading = false;
+      },
+      err => {
+       this.error(err.message);
+        this.loading = false;
+      })
+    },
+    err => {
+      this.error(err.message);
+    })
+  }
+
+  error(msg: string){
+    Swal.fire({  
+      icon: 'error',  
+      title: 'Oops...',  
+      text: msg,  
+    });
+  }
+
 }
--- src/app/services/academy.service.ts
+++ src/app/services/academy.service.ts
@@ -1,5 +1,5 @@
 import { Observable, of } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError } from 'rxjs/operators';
 
@@ -9,20 +9,32 @@ import { catchError } from 'rxjs/operators';
 })
 export class AcademyService {
   errorMsg:string;
+  
   url = 'https://portfolioapisite.herokuapp.com/api/academy';
+  urlGetToken = 'https://portfolioapisite.herokuapp.com/api/authtoken';
+  
+  body = {
+    "UserID": "usuario01",
+    "AccessKey": "94be650011cf412ca906fc335f615cdc"
+  }
+  
   constructor(private httpClient: HttpClient) { }
 
-  getInfoAcademy(): Observable<any[]>{
-    return this.httpClient.get<any[]>(this.url).pipe(
-      catchError(error => {
-        if(error.error instanceof ErrorEvent){
-          this.errorMsg = 'Error ${error.error.message}'
-        }
-        else {
-          this.errorMsg = `Error: ${error.message}`;
-        }
-        console.log(this.errorMsg);
-        return of([]);
-      }));
+  getInfoAcademy(token: string): Observable<any[]>{
+    var hd = {
+      headers: new HttpHeaders()
+        .set("Authorization", "Bearer " + token)
+     }; 
+    return this.httpClient.get<any[]>(this.url, hd);
+  }
+
+  getAuthToken(): Observable<any>{
+    var header = { 
+      headers:new HttpHeaders()
+    .set("Access-Control-Allow-Origin", "*")
+    .set("Access-Control-Allow-Methods", "PUT,GET,POST,DELETE")
+    .set("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
+    }
+    return this.httpClient.post<any>(this.urlGetToken, this.body, header);
   }
 }
